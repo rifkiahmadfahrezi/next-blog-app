4555c15ce4ecdc689af851ad22195b65d542f15b
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-09-04 17:11:29 +0700

    feat: create middleware for protecting route

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt"
+export default async function middleware(req: NextRequest){
+   const { pathname } = req.nextUrl
+   const url = new URL(req.url)
+
+   const session = await getToken({ req })
+   const redirectTo = (path: string) => NextResponse.redirect(new URL(path, url)) 
+
+   // if user already login
+   if(pathname.startsWith('/sign') && session){
+      return redirectTo('/')
+   }
+
+   // if client access dashboard page
+   if(pathname.startsWith('/dashboard')){
+      // check login status
+      if(!session) return redirectTo('/')
+      // check role
+      if(session?.role === 'admin' || session?.role === 'author'){
+         return NextResponse.next()
+      }
+      return redirectTo('/')
+   }
+}
+
+export const config = {
+   matcher: [
+      "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      "/dashboard",
+      "/sign-in",
+      "/sign-up",
+   ],
+ }
