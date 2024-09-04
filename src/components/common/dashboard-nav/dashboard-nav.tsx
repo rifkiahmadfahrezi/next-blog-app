b303a5cc290692ceb05c3d5967e308e9613dd7b3
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-09-04 17:10:55 +0700

    feat: import userDropdown

--- src/components/common/dashboard-nav/dashboard-nav.tsx
+++ src/components/common/dashboard-nav/dashboard-nav.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import {
-   DropdownMenu,
-   DropdownMenuContent,
-   DropdownMenuItem,
-   DropdownMenuLabel,
-   DropdownMenuSeparator,
-   DropdownMenuTrigger,
- } from "@/components/ui/dropdown-menu"
 import Link from 'next/link'
-import { LogOutIcon } from 'lucide-react'
 import { SidebarMobile } from '../sidebar'
 import { Button } from '@/components/ui/button'
+
+import UserDropdown from '../user-dropdown'
 
 
 const DashboardNav = () => {
@@ -30,29 +22,7 @@ const DashboardNav = () => {
                      Home
                   </Link>
             </Button>
-            <DropdownMenu>
-               <DropdownMenuTrigger asChild >
-                  <Avatar>
-                     <AvatarImage src="https://github.com/shadcn.png" />
-                     <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-               </DropdownMenuTrigger>
-               <DropdownMenuContent>
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem asChild >
-                     <Link href={'/profile'} >
-                        Profile
-                     </Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                     <span className="text-red-600 flex items-center gap-3">
-                        <LogOutIcon className='size-3'  />
-                        Logout
-                     </span>
-                  </DropdownMenuItem>
-               </DropdownMenuContent>
-            </DropdownMenu>
+            <UserDropdown />
          </div>
       </nav>
    </>
