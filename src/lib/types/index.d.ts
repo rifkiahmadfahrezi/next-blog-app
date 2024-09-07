48f766fd44b1fc84e97f0aaeb9497dbcd291233a
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-09-07 12:03:48 +0700

    feat: create User and Role types

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -8,3 +8,15 @@ export interface Category extends DBModel {
    name: string
    slug: string
 }
+
+export interface Role extends DBModel {
+   name: string
+}
+
+export interface User extends DBModel {
+   username: string
+   email: string
+   password: string
+   roleId: string
+   role?: Role
+}
