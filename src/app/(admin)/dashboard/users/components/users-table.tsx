a4a29714db57a5a6fddeb3ffbdf80fd274c918c5
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-09-07 12:01:08 +0700

    feat: create users dashboard page

---
+++ src/app/(admin)/dashboard/users/components/users-table.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import React, { useMemo } from 'react'
+import { useQuery } from "@tanstack/react-query"
+import { Card } from '@/components/ui/card'
+import { 
+   Table,
+   TableBody,
+   TableCaption,
+   TableCell,
+   TableHead,
+   TableHeader,
+   TableRow 
+} from "@/components/ui/table"
+import { USER_ROLE_ID } from '@/lib/contants'
+import { getAllUsers } from '@/services/users'
+
+import RoleDropdown from '@/components/common/role-dropdown'
+import { useSearchParams } from 'next/navigation'
+import DeleteUser from './delete-user'
+
+const UsersTable = () => {
+   const searchParams = useSearchParams()
+   const keyword : string = searchParams.get('search') || ''
+   const { data : users, isLoading } = useQuery({
+      queryKey: ['users'],
+      queryFn: () => getAllUsers(USER_ROLE_ID)
+   })
+
+   const filteredUsers = useMemo(() => {
+      return users
+      ?.filter(item => item.username.toLowerCase().includes(keyword) || item.email.toLowerCase().includes(keyword) )
+   }, [keyword, users, searchParams])
+
+  return (
+   <>
+      <Card className="p-5">
+         <Table>
+            <TableHeader>
+               <TableRow>
+                  <TableHead>No</TableHead>
+                  <TableHead>Username</TableHead>
+                  <TableHead>Email</TableHead>
+                  <TableHead>Role</TableHead>
+                  <TableHead>Actions</TableHead>
+               </TableRow>
+            </TableHeader>
+            <TableBody>
+               {isLoading && <TableRow><TableCell>Loading...</TableCell></TableRow> }
+               {!isLoading && filteredUsers?.map((item, i) => (
+                  <TableRow key={item.id} >
+                     <TableCell>{i+1}</TableCell>
+                     <TableCell>{item.username}</TableCell>
+                     <TableCell>{item.email}</TableCell>
+                     <TableCell>
+                        <RoleDropdown user={item} />
+                     </TableCell>
+                     <TableCell>
+                        <DeleteUser id={item.id.toString()} queryKey={['users']} />
+                     </TableCell>
+                  </TableRow>
+               ))}
+            </TableBody>
+            <TableCaption>
+               {!searchParams.get('search')
+                  ? <p>List of users ({users?.length || 0} items)</p>
+                  : <p>Result for &ldquo;{keyword}&rdquo; ({filteredUsers?.length || 0} items)</p>
+               }
+            </TableCaption>
+         </Table>
+      </Card>
+   </>
+  )
+}
+
+export default UsersTable
