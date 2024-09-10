706e18b0315bec781161c55c9350160cee207162
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-09-11 05:49:20 +0700

    feat: only blogs writer and role admin can update and delete blog

--- src/app/(admin)/dashboard/blogs/components/blogs-table.tsx
+++ src/app/(admin)/dashboard/blogs/components/blogs-table.tsx
@@ -13,7 +13,6 @@ import {
 import { useQuery } from "@tanstack/react-query"
 import { Card } from "@/components/ui/card"
 import { useSearchParams } from "next/navigation"
-import { Badge } from "@/components/ui/badge"
 import {
    Tooltip,
    TooltipContent,
@@ -26,11 +25,13 @@ import Link from "next/link"
 import DeleteBlog from "./delete-blog"
 import { PencilIcon, PlusIcon } from "lucide-react"
 import BlogVisibilityDropdown from "@/components/common/blog-visibility-dropdown"
-// import AddCategory from "./add-category"
+import { useSession } from "next-auth/react"
+import { Badge } from "@/components/ui/badge"
 
 import { getAllBlogs } from "@/services/blogs"
 
 export default function BlogsTable({ published } : { published: boolean }) {
+   const { data : session } = useSession()
    const searchParams = useSearchParams()
    const { data : blogs, isLoading} = useQuery({
       queryKey: published ? ['blogs-published'] : ['blogs-drafted'],
@@ -80,16 +81,24 @@ export default function BlogsTable({ published } : { published: boolean }) {
                   <TableRow key={item.id} >
                      <TableCell>{i+1}</TableCell>
                      <TableCell className="line-clamp-1" >
-                        <Link className="underline" 
-                           href={`/blogs/${item.slug}`}
-                           target="_blank"
-                           >
-                           {item.title}
-                        </Link>
+                        {item.isPublished
+                        ? <Link className="underline" 
+                              href={`/blogs/${item.slug}`}
+                              target="_blank"
+                              >
+                              {item.title}
+                           </Link>
+                        : <p>{item.title}</p> 
+                        }
                      </TableCell>
-                     <TableCell>{
-                        <BlogVisibilityDropdown blog={item} />
-                     }</TableCell>
+                     <TableCell>
+                        {
+                           // @ts-ignore
+                           (session?.email === item.user.email || session?.role === 'admin')
+                           ? <BlogVisibilityDropdown blog={item} />
+                           : item.isPublished ? <Badge>Published</Badge> : <Badge variant={'outline'}>drafted</Badge>
+                        }
+                     </TableCell>
                      <TableCell>
                         <TooltipProvider>
                            <Tooltip>
@@ -112,19 +121,27 @@ export default function BlogsTable({ published } : { published: boolean }) {
                      </TableCell>
                      <TableCell>{item.user.username}</TableCell>
                      <TableCell className="flex gap-2" >
-                        <Button asChild 
-                           size={'icon'}
-                           variant={'ghost'}
-                           >
-                           <Link 
-                              target="_blank"
-                              href={`/blogs/update/${item.slug}`}>
-                              <PencilIcon className="size-5" />
-                           </Link>
-                        </Button>
-                        <DeleteBlog 
-                           published={item.isPublished}
-                           id={item.id as string} />  
+                        {  
+                           // @ts-ignore
+                           session?.user.email === item.user.email &&
+                           <Button asChild 
+                              size={'icon'}
+                              variant={'ghost'}
+                              >
+                              <Link 
+                                 target="_blank"
+                                 href={`/blogs/update/${item.slug}`}>
+                                 <PencilIcon className="size-5" />
+                              </Link>
+                           </Button>
+                        }
+                        {
+                           // @ts-ignore
+                           (session?.user.email === item.user.email || session?.role === 'admin') &&
+                           <DeleteBlog 
+                              published={item.isPublished}
+                              id={item.id as string} />  
+                        }
                      </TableCell>
                   </TableRow>
                ))}
