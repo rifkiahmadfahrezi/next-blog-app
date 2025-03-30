51b9032e7ed5138e00f3714bb41c8c72d6e0e8c4
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-03-30 18:54:40 +0700

    chore: upgrade next,react version

--- src/components/common/progressbar-provider.tsx
+++ src/components/common/progressbar-provider.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import React from 'react'
-import { AppProgressBar } from "next-nprogress-bar"
+import { ProgressProvider } from '@bprogress/next/app'
 
 const ProgressbarProvider = ({ children } : { children: React.ReactNode }) => {
   return (
    <>
       
       {children}
-      <AppProgressBar 
+      <ProgressProvider 
          color='#FFD11A'
          height='4px'
          shallowRouting
